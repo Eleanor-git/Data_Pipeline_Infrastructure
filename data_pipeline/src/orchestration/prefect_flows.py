1c6d366ae230d38355e8ef131df17a4b39eb5cfd
Eleanor-git <n10850481@qut.edu.au>
2026-05-13 00:50:04 +0800

    containerized

--- data_pipeline/src/orchestration/prefect_flows.py
+++ data_pipeline/src/orchestration/prefect_flows.py
@@ -1,8 +1,3 @@
-"""
-A Simple Data Processing Pipeline
-Download data from S3, clean it, transform it, and upload results back to S3.
-"""
-
 import os
 import boto3
 import pandas as pd
@@ -11,76 +6,38 @@ from dotenv import load_dotenv
 from datetime import datetime
 import numpy as np
 import tempfile
+from prefect import flow, task, get_run_logger
 
 
-def process_ecommerce_data():
-    """Download, process, and upload e-commerce data"""
-
-    print("Starting data processing...")
-
-    # Load environment variables
-    load_dotenv()
-    bucket_name = os.getenv('AWS_S3_BUCKET_NAME')
-    region = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
-
-    if not bucket_name:
-        print("ERROR: AWS_S3_BUCKET_NAME not found in .env file!")
-        return False
-
-    print(f"Processing data from bucket: {bucket_name}")
-
-    try:
-        # Create S3 client
-        s3_client = boto3.client('s3', region_name=region)
-
-        # Step 1: Download data from S3
-        print("\nStep 1: Downloading data from S3...")
-        datasets = download_data_from_s3(s3_client, bucket_name)
-
-        # Step 2: Clean and transform data
-        print("\nStep 2: Cleaning and transforming data...")
-        processed_datasets = transform_data(datasets)
-
-        # Step 3: Upload processed data back to S3
-        print("\nStep 4: Uploading processed data to S3...")
-        upload_success = upload_processed_data(s3_client, bucket_name, processed_datasets)
-
-        if upload_success:
-            print("\nSUCCESS: Data processing pipeline completed!")
-            return True
-        else:
-            print("\nERROR: Failed to upload processed data")
-            return False
-
-    except Exception as e:
-        print(f"ERROR: Data processing failed: {e}")
-        return False
-
-
+@task(name="dowload_data_from_s3", retries=2, retry_delay_seconds=30, cache_policy=None)
 def download_data_from_s3(s3_client, bucket_name):
+    logger = get_run_logger()
+    logger.info("Starting data download from s3 bucket.")
+
     dataset_dict = {}
-    #data_files = os.listdir("../../data")
-    data_files = ['customers.csv', 'order_items.csv','orders.csv', 'products.csv','reviews.csv']
+    data_files = os.listdir("../../../data") # broken from here
 
     for f_name in data_files:
         try:
             print(f"Downloading {f_name}...")
-            local_path = os.path.join("../../Download", f_name)
+            local_path = os.path.join(tempfile.gettempdir(), f_name)
 
             s3_client.download_file(bucket_name, f"data/{f_name}", local_path)
             df = pd.read_csv(local_path)
             dict_key = f_name.replace(".csv", "")
             dataset_dict[dict_key] = df
 
-            print(f"[SUCCESS] {dict_key} has been loaded with records: {len(df)}\n")
+            logger.info(f"[SUCCESS] {dict_key} has been loaded with records: {len(df)}\n")
             os.remove(local_path)
 
         except Exception as e:
-            print(f"Failed to download {f_name} with: {e}\n")
+            logger.error(f"Failed to download {f_name} with: {e}\n")
     return dataset_dict
 
 
+@task(name="transform_data", retries=1)
 def transform_data(datasets):
+    logger = get_run_logger()
     processed = {}
 
     # "customers" pre-processing
@@ -103,7 +60,7 @@ def transform_data(datasets):
                                         bins=np.linspace(0, 100, 6),  # [  0.,  20.,  40.,  60.,  80., 100.]
                                         labels=['0-20', '21-40', '41-60', '61-80', '80+'])
         processed['cleaned_customers'] = customers
-        print(f"\"Customers\" pre-processing completed with shape: {customers.shape}\n")
+        logger.info(f"\"Customers\" pre-processing completed with shape: {customers.shape}")
 
     # "products" pre-processing
     if "products" in datasets:
@@ -124,7 +81,7 @@ def transform_data(datasets):
                                             bins=np.linspace(np.min(products['price']), np.max(products['price']), 5),
                                             labels=['Budget', 'Medium', 'Premium', 'Luxury'])
         processed['cleaned_products'] = products
-        print(f"\"Products\" pre-processing completed with shape: {products.shape}\n")
+        logger.info(f"\"Products\" pre-processing completed with shape: {products.shape}")
 
     # "orders" pre-processing
     if 'orders' in datasets:
@@ -141,7 +98,7 @@ def transform_data(datasets):
         orders["order_month"] = orders["order_date"].dt.month
 
         processed['cleaned_orders'] = orders
-        print(f"\"Orders\" pre-processing completed with shape: {orders.shape}\n")
+        logger.info(f"\"Orders\" pre-processing completed with shape: {orders.shape}")
 
     # "order_items" pre-processing
     if 'order_items' in datasets:
@@ -155,7 +112,7 @@ def transform_data(datasets):
         # Calculating total price of each item
         order_items['total_price'] = order_items['unit_price'] * order_items['quantity']
         processed['cleaned_order_items'] = order_items
-        print(f"\"Order_items\" pre-processing completed with shape: {order_items.shape}\n")
+        logger.info(f"\"Order_items\" pre-processing completed with shape: {order_items.shape}")
 
     # "reviews" pre-processing
     if 'reviews' in datasets:
@@ -174,12 +131,14 @@ def transform_data(datasets):
         'Poor')
 
         processed['cleaned_reviews'] = reviews
-        print(f'Processed reviews: {reviews.shape} records')
+        logger.info(f'Processed reviews: {reviews.shape} records')
 
     return processed
 
 
+@task(name="upload_processed_data", retries=2, retry_delay_seconds=5, cache_policy=None)
 def upload_processed_data(s3_client, bucket_name, processed):
+    logger = get_run_logger()
     upload_count = 0
     total_files = len(processed)  # + len(metrics)
 
@@ -192,7 +151,7 @@ def upload_processed_data(s3_client, bucket_name, processed):
             s3_key = f"processed/{dataset_name}.csv"
             s3_client.upload_file(local_path, bucket_name, s3_key)
             # s3_client.upload_file(local_path, xxx, s3_key) # BROKEN
-            print(f"Uploaded {dataset_name} with shape {df.shape}\n")
+            logger.info(f"Uploaded {dataset_name} with shape {df.shape}")
 
             upload_count += 1
 
@@ -200,9 +159,55 @@ def upload_processed_data(s3_client, bucket_name, processed):
             os.remove(local_path)
 
         except Exception as e:
-            print(f"Failed to upload {dataset_name}.\n[ERROR]: {e}\n")
+            logger.error(f"Failed to upload {dataset_name}.\n[ERROR]: {e}")
 
     return upload_count == total_files
+
+
+@flow(name="ecommerce_ETL_pipeline")
+def process_ecommerce_data():
+    """Download, process, and upload e-commerce data"""
+
+    logger = get_run_logger()
+    logger.info("Starting data processing...")
+
+    # Load environment variables
+    load_dotenv()
+    bucket_name = os.getenv('AWS_S3_BUCKET_NAME')
+    region = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
+
+    if not bucket_name:
+        logger.error("ERROR: AWS_S3_BUCKET_NAME not found in .env file!")
+        return False
+
+    logger.info(f"Processing data from bucket: {bucket_name}")
+
+    try:
+        # Create S3 client
+        s3_client = boto3.client('s3', region_name=region)
+
+        # Step 1: Download data from S3
+        logger.info("\nStep 1: Downloading data from S3...")
+        datasets = download_data_from_s3(s3_client, bucket_name)
+
+        # Step 2: Clean and transform data
+        logger.info("\nStep 2: Cleaning and transforming data...")
+        processed_datasets = transform_data(datasets)
+
+        # Step 3: Upload processed data back to S3
+        logger.info("\nStep 3: Uploading processed data to S3...")
+        upload_success = upload_processed_data(s3_client, bucket_name, processed_datasets)
+
+        if upload_success:
+            logger.info("\nSUCCESS: Data processing pipeline completed!")
+            return True
+        else:
+            logger.error("\nERROR: Failed to upload processed data")
+            return False
+
+    except Exception as e:
+        print(f"ERROR: Data processing failed: {e}")
+        return False
 
 
 if __name__ == "__main__":
